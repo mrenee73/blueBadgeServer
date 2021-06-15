3affafe4aaa03c71aecf9df4d6f31bbac004bcb8
kcmjschwartz <kcmjschwartz@gmail.com>
2021-06-15 14:10:12 -0400

    change sequelize back

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL || `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/hoa-log`,{
     dialect: 'postgres',
     ssl: process.env.ENVIRONMENT === 'production'
 })
 
-//comment
+//commentno
 
 module.exports = sequelize; 
