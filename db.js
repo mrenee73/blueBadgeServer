e3e705af6b1587f00d234015b1c0409e46a9a05f
kcmjschwartz <kcmjschwartz@gmail.com>
2021-06-15 13:15:44 -0400

    change db

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING || `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/hoa-log`, {
+const sequelize = new Sequelize(process.env.DATABASE_URL || `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/hoa-log`, {
     dialect: 'postgres',
     ssl: process.env.ENVIRONMENT === 'production'
 })
