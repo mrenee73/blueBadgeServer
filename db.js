7607e82a99fb71fe08df93cb3e6bb1c65f7fedc8
kcmjschwartz <kcmjschwartz@gmail.com>
2021-06-15 13:58:32 -0400

    typo

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequelize');
+const sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL || `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/hoa-log`,{
     dialect: 'postgres',
